127843fe18413180b1ea6307fc89a48cd47ed5dc
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-11-08 11:19:38 +0700

    create admin layout

---
+++ src/components/Common/PrivateRoute.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface PrivateRouteProps {}
+
+const PrivateRoute = (props: PrivateRouteProps) => {
+  return <div>private route </div>;
+};
+
+export default PrivateRoute;
---
+++ src/components/Layout/Admin.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface AdminLayoutProps {}
+
+const AdminLayout = (props: AdminLayoutProps) => {
+  return <div></div>;
+};
+
+export default AdminLayout;
